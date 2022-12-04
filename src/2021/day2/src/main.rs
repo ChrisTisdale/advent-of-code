a2dd7823ca2a36fd8e62b2fd910bc1ef51d5ac45
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-04 17:46:51 -0600

    Adding Advent of Code 2022 day 1.
    C# functions correctly rust is just 2021 still needs to be fixed

---
+++ src/2021/day2/src/main.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main() {
+    let mut count: i32 = 0;
+
+    let data = fs::read_to_string("assets/measurements.txt")
+        .unwrap()
+        .lines()
+        .filter(|s | !s.is_empty())
+        .map(|s | {
+            s.parse::<i32>().unwrap()
+        }).collect::<Vec<i32>>();
+
+    println!("{}", count)
+}
