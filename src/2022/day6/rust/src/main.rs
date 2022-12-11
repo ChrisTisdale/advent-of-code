6cb16c7c7b04602ed9f97359bf1ac4b904814307
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-11 09:41:06 -0600

    Adding day6 implementation rust only

---
+++ src/2022/day6/rust/src/main.rs
@@ -0,0 +1,39 @@
+use std::collections::{HashMap, VecDeque};
+use std::fs;
+
+fn main()
+{
+    for d in fs::read_to_string("assets/measurements.txt")
+        .unwrap()
+        .lines()
+        .filter(|s | !s.is_empty())
+    {
+        let mut m = HashMap::new();
+        let mut l = VecDeque::new();
+        let mut count: i32 = 0;
+        println!("Checking: {}", d);
+        for c in d.chars().enumerate()
+        {
+            count += 1;
+            if m.contains_key(&c.1)
+            {
+                let mut x = l.pop_front().unwrap();
+                m.remove(&x);
+                while x != c.1
+                {
+                    x = l.pop_front().unwrap();
+                    m.remove(&x);
+                }
+            }
+
+            m.insert(c.1, 0);
+            l.push_back(c.1);
+            if m.keys().count() == 14
+            {
+                break;
+            }
+        }
+
+        println!("{}", count);
+    }
+}
