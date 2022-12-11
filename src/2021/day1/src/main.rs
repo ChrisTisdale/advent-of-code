fd2ba252d52f86b55cf07802ef625890ac6646b1
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-11 09:40:50 -0600

    Adding Day 1 2021 solution

---
+++ src/2021/day1/src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+fn main() {
+    let mut count: i32 = 0;
+
+    let data = fs::read_to_string("assets/measurements.txt")
+        .unwrap()
+        .lines()
+        .filter(|s | !s.is_empty())
+        .map(|s | {
+            s.parse::<i32>().unwrap()
+        }).collect::<Vec<i32>>();
+
+    for (i, _value) in data.iter().enumerate() {
+        if i > data.len() - 4 {
+            break;
+        }
+
+        let pre_total: i32 = data[i..(i + 3)].iter().sum();
+        let cur_total: i32 = data[(i + 1)..(i + 4)].iter().sum();
+        if pre_total < cur_total {
+            count = count + 1;
+        }
+    }
+
+    println!("{}", count)
+}
