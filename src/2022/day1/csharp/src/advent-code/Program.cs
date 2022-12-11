ffbdc4821599bc47ead013529ef80ec9b54bb439
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-11 09:40:53 -0600

    Adding Advent of Code 2022 day 1.
    C# functions correctly rust is just 2021 still needs to be fixed

---
+++ src/2022/day1/csharp/src/advent-code/Program.cs
@@ -0,0 +1,30 @@
+﻿var result = await FindBest("sample.txt");
+Console.WriteLine($"Sample Found max: {result}");
+
+result = await FindBest("measurements.txt");
+Console.WriteLine($"Result Found max: {result}");
+
+async ValueTask<decimal> FindBest(string filename)
+{
+    var cur = 0m;
+    var values = new List<decimal>();
+    await foreach (var readLine in File.ReadLinesAsync(filename))
+    {
+        if (string.IsNullOrWhiteSpace(readLine))
+        {
+            values.Add(cur);
+            cur = 0;
+        }
+        else
+        {
+            cur += decimal.Parse(readLine);
+        }
+    }
+
+    if (cur != 0)
+    {
+        values.Add(cur);
+    }
+
+    return values.OrderDescending().Take(3).Sum();
+}
