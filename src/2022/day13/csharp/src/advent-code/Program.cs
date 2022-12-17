b8400389675969ffd8e974f46e8f5ee1ca9f359b
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-17 09:43:25 -0600

    Adding day13 2022 implementing c#

---
+++ src/2022/day13/csharp/src/advent-code/Program.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Text;
+using System.Text.RegularExpressions;
+
+var regex = new Regex($"({Regex.Escape(",")}|{Regex.Escape("[")}|{Regex.Escape("]")})");
+
+await HandleFile("sample.txt");
+await HandleFile("measurements.txt");
+
+async ValueTask HandleFile(string file)
+{
+    var result = await GetPackets(file);
+    var starter = new ListPacket(new []{ new ListPacket(new[] { new ValuePacket(2) }) });
+    var ender = new ListPacket(new []{ new ListPacket(new[] { new ValuePacket(6) }) });
+    var sortedList =
+        new SortedSet<IPacket>(result.SelectMany(x => x).Append(starter).Append(ender), new PacketComparer());
+    var count = 0;
+    var startIndex = 0;
+    var endIndex = 0;
+    foreach (var p in sortedList)
+    {
+        ++count;
+        if (Equals(p, starter))
+        {
+            startIndex = count;
+        }
+
+        if (Equals(p, ender))
+        {
+            endIndex = count;
+        }
+    }
+    
+    Console.WriteLine($"{file} has start {startIndex} and end {endIndex} with an answer of {startIndex * endIndex}");
+    //Console.WriteLine(string.Join(Environment.NewLine, sortedList));
+}
+
+async ValueTask<IReadOnlyList<PacketChecker>> GetPackets(string file)
+{
+    var lines = await File.ReadAllLinesAsync(file);
+    var comparisions = new List<PacketChecker>();
+    for (var i = 0; i < lines.Length; i+=3)
+    {
+        var left = ParseLine(lines[i]);
+        var right = ParseLine(lines[i + 1]);
+        var packetChecker = new PacketChecker(left, right);
+        //Console.WriteLine(packetChecker);
+        comparisions.Add(packetChecker);
+    }
+
+    return comparisions;
+}
+
+ListPacket ParseLine(string line)
+{
+    var values = regex.Split(line).Where(x => !string.IsNullOrEmpty(x)).ToArray();
+    var count = 1; // skip first is must always be [
+    return ParseValues(values, ref count);
+}
+
+ListPacket ParseValues(IReadOnlyList<string> values, ref int i)
+{
+    var data = new List<IPacket>();
+    for (; i < values.Count; ++i)
+    {
+        var value = values[i];
+        switch (value)
+        {
+            case "[":
+                ++i;
+                data.Add(ParseValues(values, ref i));
+                break;
+            case "]":
+                ++i;
+                return new ListPacket(data);
+            case ",":
+                break;
+            default:
+                data.Add(new ValuePacket(int.Parse(value)));
+                break;
+        }
+    }
+
+    return new ListPacket(data);
+}
+
+interface IPacket
+{
+}
+
+record ListPacket(IReadOnlyList<IPacket> Values) : IPacket
+{
+    protected virtual bool PrintMembers(StringBuilder builder)
+    {
+        builder.Append(nameof(Values)).Append(" = ").Append(" [ ").Append(string.Join(",", Values)).Append(" ]");
+        return true;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append('[').Append(string.Join(",", Values)).Append(']');
+        return sb.ToString();
+    }
+}
+
+record ValuePacket(int Value) : IPacket
+{
+    public override string ToString()
+    {
+        return Value.ToString();
+    }
+}
+
+record PacketChecker(IPacket Left, IPacket Right) : IEnumerable<IPacket>
+{
+    public IEnumerator<IPacket> GetEnumerator()
+    {
+        yield return Left;
+        yield return Right;
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class PacketComparer : IComparer<IPacket>
+{
+    public int Compare(IPacket? x, IPacket? y)
+    {
+        return InCorrectOrder(x ?? throw new ArgumentNullException(nameof(x)),
+            y ?? throw new ArgumentNullException(nameof(y)));
+    }
+
+    private static int InCorrectOrder(IPacket left, IPacket right)
+    {
+        switch (left)
+        {
+            case ValuePacket valLeft when right is ValuePacket valRight:
+                return valLeft.Value.CompareTo(valRight.Value);
+            case ListPacket listLeft when right is ListPacket listRight:
+            {
+                for (var i = 0; i < Math.Min(listLeft.Values.Count, listRight.Values.Count); ++i)
+                {
+                    var leftValue = listLeft.Values[i];
+                    var rightValue = listRight.Values[i];
+                    var res = InCorrectOrder(leftValue, rightValue);
+                    if (res != 0)
+                    {
+                        return res;
+                    }
+                }
+
+                return listLeft.Values.Count.CompareTo(listRight.Values.Count);
+            }
+            default:
+                return left is ValuePacket
+                    ? InCorrectOrder(new ListPacket(new[] { left }), right)
+                    : InCorrectOrder(left, new ListPacket(new[] { right }));
+        }
+    }
+}
