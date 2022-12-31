6f7ec7e8af29b9f8c29428139b57d04040ee9628
Chris Tisdale <cjtisdale21@gmail.com>
2022-12-31 10:06:43 -0600

    Removing dead code

--- src/2022-csharp/day6/Day6.cs
+++ src/2022-csharp/day6/Day6.cs
@@ -55,32 +55,4 @@ public class Day6 : Base2022AdventOfCodeDay<Results>
 
         return count;
     }
-
-    private static async ValueTask<Results> FindSubsets(string filename, bool anyOverlap)
-    {
-        var count = 0;
-        await foreach (var line in File.ReadLinesAsync(filename))
-        {
-            var split = line.Split(',', '-').Select(int.Parse).ToArray();
-            if (split.Length != 4)
-            {
-                continue;
-            }
-
-            var range1 = Enumerable.Range(split[0], split[1] - split[0] + 1).ToArray();
-            var range2 = Enumerable.Range(split[2], split[3] - split[2] + 1).ToArray();
-            if (anyOverlap ? AnyOverlap(range1, range2) : AllOverlap(range1, range2))
-            {
-                count++;
-            }
-        }
-
-        return new Results(count);
-    }
-
-    private static bool AnyOverlap(int[] range1, int[] range2) =>
-        range1.Any(range2.Contains);
-
-    private static bool AllOverlap(int[] range1, int[] range2) =>
-        range1.All(range2.Contains) || range2.All(range1.Contains);
 }
