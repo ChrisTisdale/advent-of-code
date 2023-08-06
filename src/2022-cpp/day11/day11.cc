f65c6716d8e39bf133abde4e0e637afb6cf7fc73
Chris Tisdale <cjtisdale21@gmail.com>
2023-08-06 12:09:19 -0500

    Code Cleanup for day11 c++

--- src/2022-cpp/day11/day11.cc
+++ src/2022-cpp/day11/day11.cc
@@ -1,6 +1,7 @@
 #include "day11.h"
 
 #include <algorithm>
+#include <numeric>
 #include <sstream>
 
 using namespace day11;
@@ -15,109 +16,115 @@ long long solution::run_part2(std::istream& file) {
   return run_game(result, false, 10000);
 }
 
-std::vector<std::shared_ptr<monkey>> solution::read_file(std::istream& file) {
-  std::vector<std::shared_ptr<monkey>> monkeys;
+long long solution::get_input(operations type, std::string& line) {
+  size_t index;
+  switch (type) {
+    case add:
+      index = line.find('+');
+      break;
+    case multiply:
+      index = line.find('*');
+      break;
+    case subtract:
+      index = line.find('-');
+      break;
+    default:
+      index = line.find('/');
+      break;
+  }
+
+  char* end;
+  auto search = line.find("old", index + 1);
+  if (search != std::string::npos) {
+    return -1;
+  } else {
+    return strtoll(line.c_str() + index + 1, &end, 10);
+  }
+}
+
+long long solution::get_divisor(std::string& line) {
+  auto index = line.find(" by ") + 4;
+  char* end;
+  return strtoll(line.c_str() + index, &end, 10);
+}
+
+size_t solution::get_false_value(std::string& line) {
+  auto index = line.find("to monkey ") + 10;
+  char* end;
+  return strtoll(line.c_str() + index, &end, 10);
+}
+
+size_t solution::get_true_value(std::string& line) {
+  auto index = line.find("to monkey ") + 10;
+  char* end;
+  return strtoll(line.c_str() + index, &end, 10);
+}
+
+operations solution::get_operation_type(std::string& line) {
+  if (line.contains('+')) return add;
+  if (line.contains('*')) return multiply;
+  if (line.contains('-')) return subtract;
+  return divide;
+}
+
+monkey solution::build_monkey(std::istream& file,
+                              std::queue<long long>&& init) {
+  std::string line;
+  std::getline(file, line);
+  auto type = get_operation_type(line);
+  auto value = get_input(type, line);
+  std::getline(file, line);
+  auto divisor = get_divisor(line);
+  std::getline(file, line);
+  auto true_value = get_true_value(line);
+  std::getline(file, line);
+  auto false_value = get_false_value(line);
+  return {std::move(init),
+          operation(false_value, true_value, divisor, value, type)};
+}
+
+std::queue<long long> solution::get_start(std::string& line) {
+  std::queue<long long> init;
+  std::stringstream ss(line);
+  std::string temp;
+  std::getline(ss, temp, ':');
+  char* end;
+  while (std::getline(ss, temp, ',')) {
+    auto value = strtoll(temp.c_str(), &end, 10);
+    init.push(value);
+  }
+
+  return init;
+}
+
+std::vector<monkey> solution::read_file(std::istream& file) {
+  std::vector<monkey> monkeys;
   std::string line;
   while (true) {
-    std::queue<long long> init;
-    for (int i = 0; i < 3; ++i) {
-      if (!std::getline(file, line)) {
-        return monkeys;
-      }
-
-      switch (i) {
-        case 1: {
-          std::stringstream ss(line);
-          std::string temp;
-          std::getline(ss, temp, ':');
-          while (std::getline(ss, temp, ',')) {
-            char* end;
-            auto value = strtoll(temp.c_str(), &end, 10);
-            init.push(value);
-          }
-        } break;
-        case 2: {
-          // op
-          bool is_add = line.contains("+");
-          size_t index;
-          if (is_add) {
-            index = line.find('+');
-          } else {
-            index = line.find('*');
-          }
-
-          long long value;
-          char* end;
-          auto search = line.find("old", index + 1);
-          if (search != std::string::npos) {
-            value = -1;
-          } else {
-            value = strtoll(line.c_str() + index + 1, &end, 10);
-          }
-
-          if (!std::getline(file, line)) {
-            return monkeys;
-          }
-
-          // test
-          index = line.find(" by ") + 4;
-          long long divisor = strtoll(line.c_str() + index, &end, 10);
-          if (!std::getline(file, line)) {
-            return monkeys;
-          }
-
-          // true value
-          index = line.find("to monkey ") + 10;
-          std::size_t true_value = strtoll(line.c_str() + index, &end, 10);
-          if (!std::getline(file, line)) {
-            return monkeys;
-          }
-
-          // false value
-          index = line.find("to monkey ") + 10;
-          std::size_t false_value = strtoll(line.c_str() + index, &end, 10);
-          if (is_add) {
-            monkeys.emplace_back(std::make_shared<monkey>(
-                init, std::make_unique<add_operator>(value, false_value,
-                                                     true_value, divisor)));
-          } else {
-            monkeys.emplace_back(std::make_shared<monkey>(
-                init, std::make_unique<multiply_operator>(
-                          value, false_value, true_value, divisor)));
-          }
-        } break;
-        default:
-          break;
-      }
+    if (!std::getline(file, line) || !std::getline(file, line)) {
+      return monkeys;
     }
 
+    monkeys.push_back(build_monkey(file, get_start(line)));
     std::getline(file, line);
   }
 }
 
-long long solution::run_game(const std::vector<std::shared_ptr<monkey>>& result,
-                             bool damage, int loop_count) {
-  std::vector<long long> inspected;
-  for (std::size_t i = 0; i < result.size(); ++i) {
-    inspected.push_back(0);
-  }
-
+long long solution::run_game(std::vector<monkey>& result, bool damage,
+                             int loop_count) {
+  long long inspected[result.size()];
+  std::memset(inspected, 0, sizeof(inspected));
   long long factor = damage ? 1 : get_factor(result);
   for (int i = 0; i < loop_count; ++i) {
     size_t index = 0;
-    for (const auto& m : result) {
-      while (!m->items.empty()) {
-        auto value = m->items.front();
-        m->items.pop();
-        value = m->handler->get_result(value);
-        if (damage) {
-          value /= 3;
-        } else {
-          value = value % factor;
-        }
-
-        auto pass = m->handler->eval(value);
-        result[pass]->items.push(value);
+    for (auto& m : result) {
+      while (!m.items.empty()) {
+        auto value = m.items.front();
+        m.items.pop();
+        value = m.handler.get_result(value);
+        value = damage ? value / 3 : value % factor;
+        auto pass = m.handler.eval(value);
+        result[pass].items.push(value);
         inspected[index]++;
       }
 
@@ -125,19 +132,15 @@ long long solution::run_game(const std::vector<std::shared_ptr<monkey>>& result,
     }
   }
 
-  std::sort(inspected.begin(), inspected.end(),
-            [](auto l, auto r) { return l > r; });
+  std::sort(inspected, inspected + result.size(),
+            [](const auto l, const auto r) { return l > r; });
   long long first = inspected[0];
   long long second = inspected[1];
   return first * second;
 }
 
-long long solution::get_factor(
-    const std::vector<std::shared_ptr<monkey>>& result) {
-  long long factor = 1ll;
-  for (const auto& m : result) {
-    factor *= m->handler->get_divisor();
-  }
-
-  return factor;
+long long solution::get_factor(const std::vector<monkey>& result) {
+  return std::accumulate(
+      result.begin(), result.end(), 1ll,
+      [](const auto l, const auto& r) { return l * r.handler.get_divisor(); });
 }
--- src/2022-cpp/day11/day11.h
+++ src/2022-cpp/day11/day11.h
@@ -6,6 +6,8 @@
 #include <vector>
 
 namespace day11 {
+enum operations { add, multiply, subtract, divide };
+
 class operation {
  public:
   [[nodiscard]] std::size_t eval(long long value) const {
@@ -16,64 +18,57 @@ class operation {
     return true_throw;
   }
 
-  [[nodiscard]] virtual long long get_result(long long value) = 0;
+  [[nodiscard]] long long get_result(long long value) {
+    switch (type) {
+      case add:
+        return input < 0 ? value + value : input + value;
+      case multiply:
+        return input < 0 ? value * value : input * value;
+      case subtract:
+        return input < 0 ? 0 : input - value;
+      default:
+        return input < 0 ? 1 : input / value;
+    }
+  };
 
   [[nodiscard]] long long get_divisor() const { return divisor; }
 
-  virtual ~operation() = default;
+  operation(std::size_t false_throw, std::size_t true_throw, long long divisor,
+            long long input, operations type)
+      : false_throw(false_throw),
+        true_throw(true_throw),
+        divisor(divisor),
+        input(input),
+        type(type) {}
 
- protected:
-  operation(std::size_t false_throw, std::size_t true_throw, long long divisor)
-      : false_throw(false_throw), true_throw(true_throw), divisor(divisor) {}
+  operation(const operation& o) = delete;
+
+  operation(operation&& o) noexcept
+      : false_throw(o.false_throw),
+        true_throw(o.true_throw),
+        divisor(o.divisor),
+        input(o.input),
+        type(o.type) {}
 
  private:
   std::size_t false_throw;
   std::size_t true_throw;
   long long divisor;
-};
-
-class add_operator : public operation {
- public:
-  explicit add_operator(long long input, std::size_t false_throw,
-                        std::size_t true_throw, long long divisor)
-      : input(input), operation(false_throw, true_throw, divisor) {}
-
- protected:
-  long long get_result(long long value) override {
-    return input < 0 ? value + value : input + value;
-  }
-
- private:
   long long input;
+  operations type;
 };
 
-class multiply_operator : public operation {
- public:
-  explicit multiply_operator(long long input, std::size_t false_throw,
-                             std::size_t true_throw, long long divisor)
-      : input(input), operation(false_throw, true_throw, divisor) {}
-
- protected:
-  long long get_result(long long value) override {
-    return input < 0 ? value * value : input * value;
-  }
-
- private:
-  long long input;
-};
-
-class monkey {
- public:
-  monkey(std::queue<long long> items, std::unique_ptr<operation> handler)
+struct monkey {
+  monkey(std::queue<long long> items, operation handler)
       : items(std::move(items)), handler(std::move(handler)) {}
+
   monkey(const monkey& o) = delete;
-  monkey(monkey&& o) noexcept {
-    items = std::move(o.items);
-    handler = std::move(o.handler);
-  }
+
+  monkey(monkey&& o) noexcept
+      : items(std::move(o.items)), handler(std::move(o.handler)) {}
 
   std::queue<long long> items;
-  std::unique_ptr<operation> handler;
+  operation handler;
 };
 
 class solution {
@@ -82,10 +77,17 @@ class solution {
   static long long run_part2(std::istream& file);
 
  private:
-  static std::vector<std::shared_ptr<monkey>> read_file(std::istream& file);
-  static long long run_game(const std::vector<std::shared_ptr<monkey>>& result,
-                            bool damage, int loop_count);
-  static long long get_factor(
-      const std::vector<std::shared_ptr<monkey>>& result);
+  static std::vector<monkey> read_file(std::istream& file);
+  static long long run_game(std::vector<monkey>& result, bool damage,
+                            int loop_count);
+  static long long get_factor(const std::vector<monkey>& result);
+  static monkey build_monkey(std::istream& file,
+                             std::queue<long long int>&& init);
+  static std::queue<long long> get_start(std::string& line);
+  static long long get_input(operations type, std::string& line);
+  static long long get_divisor(std::string& line);
+  static size_t get_false_value(std::string& line);
+  static size_t get_true_value(std::string& line);
+  static operations get_operation_type(std::string& line);
 };
 }  // namespace day11
