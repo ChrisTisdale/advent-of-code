2cd0ae3701ae2c62b03697a9e02a29945ecbdc36
Chris Tisdale <cjtisdale21@gmail.com>
2024-12-08 12:43:42 -0600

    Implementing Day 7

---
+++ src/csharp/src/2024-csharp/day7/Day7.cs
@@ -0,0 +1,163 @@
+// Copyright (c) Christopher Tisdale 2024.
+//
+// Licensed under BSD-3-Clause.
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://spdx.org/licenses/BSD-3-Clause.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace AdventOfCode2024.day7;
+
+public enum Operation
+{
+    Add,
+    Multiply,
+    Concatenate,
+}
+
+public interface IOperation
+{
+    Operation Operation { get; }
+
+    public ulong Handle();
+}
+
+public class AddOperation : IOperation
+{
+    private readonly IOperation? _left;
+    private readonly ulong _right;
+
+    public AddOperation(IOperation? left, ulong right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public Operation Operation => Operation.Add;
+
+    public ulong Handle()
+    {
+        return (_left?.Handle() ?? 0) + _right;
+    }
+}
+
+public class MultiplyOperation : IOperation
+{
+    private readonly IOperation? _left;
+    private readonly ulong _right;
+
+    public MultiplyOperation(IOperation? left, ulong right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public Operation Operation => Operation.Multiply;
+
+    public ulong Handle()
+    {
+        return (_left?.Handle() ?? 1) * _right;
+    }
+}
+
+public class ConcatenateOperation : IOperation
+{
+    private readonly IOperation? _left;
+    private readonly ulong _right;
+
+    public ConcatenateOperation(IOperation? left, ulong right)
+    {
+        _left = left;
+        _right = right;
+    }
+
+    public Operation Operation => Operation.Concatenate;
+
+    public ulong Handle()
+    {
+        return ulong.Parse($"{_left?.Handle() ?? 0}{_right}");
+    }
+}
+
+public sealed record Calibration(ulong Expected, IReadOnlyList<IOperation> Operations);
+
+public class Day7 : Base2024AdventOfCodeDay<ulong>
+{
+    public override ValueTask<ulong> ExecutePart1(Stream stream, CancellationToken token = default)
+    {
+        return GetResults(stream, [Operation.Multiply, Operation.Add], token);
+    }
+
+    public override ValueTask<ulong> ExecutePart2(Stream stream, CancellationToken token = default)
+    {
+        return GetResults(stream, [Operation.Multiply, Operation.Concatenate, Operation.Add], token);
+    }
+
+    private async ValueTask<ulong> GetResults(Stream stream, Operation[] operations, CancellationToken token)
+    {
+        var calibrations = await ReadOperations(stream, operations, token);
+        return calibrations
+            .Where(
+                calibration => calibration.Operations.Select(operation => operation.Handle())
+                    .Any(calculated => calibration.Expected == calculated))
+            .Aggregate<Calibration, ulong>(0, (current, calibration) => current + calibration.Expected);
+    }
+
+    private async ValueTask<IReadOnlyList<Calibration>> ReadOperations(Stream stream, Operation[] operations, CancellationToken token)
+    {
+        var calibrations = new List<Calibration>();
+        await foreach (var line in EnumerateLinesAsync(stream, token))
+        {
+            var strings = line.Split(':');
+            if (strings.Length < 2)
+            {
+                continue;
+            }
+
+            var expected = ulong.Parse(strings[0]);
+            var count = 0;
+            var ops = new Dictionary<int, IReadOnlyList<IOperation>>();
+            foreach (var value in strings[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(ulong.Parse))
+            {
+                if (ops.TryGetValue(count - 1, out var previous))
+                {
+                    ops[count++] = previous.SelectMany(p => BuildOperations(operations, p, value)).ToList();
+                }
+                else
+                {
+                    ops[count++] = BuildOperations(operations, null, value).ToList();
+                }
+            }
+
+            if (ops.TryGetValue(count - 1, out var op))
+            {
+                calibrations.Add(new Calibration(expected, op));
+            }
+        }
+
+        return calibrations;
+    }
+
+    private IEnumerable<IOperation> BuildOperations(Operation[] operations, IOperation? previous, ulong value)
+    {
+        return operations.Select(
+                x =>
+                {
+                    IOperation o = x switch
+                    {
+                        Operation.Add => new AddOperation(previous, value),
+                        Operation.Multiply => new MultiplyOperation(previous, value),
+                        Operation.Concatenate => new ConcatenateOperation(previous, value),
+                        _ => throw new NotImplementedException()
+                    };
+
+                    return o;
+                });
+    }
+}
---
+++ src/csharp/tests/advent-code-2024Tests/day7/Day7Tests.cs
@@ -0,0 +1,57 @@
+// Copyright (c) Christopher Tisdale 2024.
+//
+// Licensed under BSD-3-Clause.
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://spdx.org/licenses/BSD-3-Clause.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace AdventOfCode2024Tests.day7;
+
+using AdventOfCode2024.day7;
+
+public class Day7Tests
+{
+    private readonly Day7 _target = new();
+
+    [Fact]
+    public void YearTest()
+    {
+        var date = _target.Year;
+        date.Should().Be(Constants.ExpectedYear);
+    }
+
+    [Fact(Timeout = 1000)]
+    public async Task Sample_Part_1_Matches()
+    {
+        var part1Result = await _target.ExecutePart1(_target.GetFileStream("sample.txt"));
+        part1Result.Should().Be(3749UL);
+    }
+
+    [Fact(Timeout = 1000)]
+    public async Task Sample_Part_2_Matches()
+    {
+        var part1Result = await _target.ExecutePart2(_target.GetFileStream("sample.txt"));
+        part1Result.Should().Be(11387UL);
+    }
+
+    [Fact(Timeout = 2000)]
+    public async Task Measurements_Part_1_Matches()
+    {
+        var part1Result = await _target.ExecutePart1(_target.GetFileStream("measurements.txt"));
+        part1Result.Should().Be(1298300076754UL);
+    }
+
+    [Fact(Skip = "Test is too long")]
+    public async Task Measurements_Part_2_Matches()
+    {
+        var part1Result = await _target.ExecutePart2(_target.GetFileStream("measurements.txt"));
+        part1Result.Should().Be(248427118972289UL);
+    }
+}
