d8944fdf39a1068cfc4a5783e8093a75199ba572
Chris Tisdale <cjtisdale21@gmail.com>
2025-12-01 19:33:48 -0600

    Adding day 1 2025

---
+++ src/rust/src/rust-2025/src/lib.rs
@@ -0,0 +1,15 @@
+// Copyright (c) Christopher Tisdale 2025.
+//
+// Licensed under BSD-3-Clause.
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://spdx.org/licenses/BSD-3-Clause.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+pub mod day1;
